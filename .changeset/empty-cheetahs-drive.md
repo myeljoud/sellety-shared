0f33bd5bb1cd9941fedc8e027bd8551e2307455c
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2023-10-21 18:43:01 +0000

    reconfiguring exports to expose folders

---
+++ .changeset/empty-cheetahs-drive.md
@@ -0,0 +1,5 @@
+---
+"sellety-shared": patch
+---
+
+Reconfiguring exports to expose `shopify` and `sanity` folders.
---
+++ tsup.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, Options } from "tsup";
+
+const commonConfig: Options = {
+  minify: true,
+  dts: true,
+  format: ["esm", "cjs"],
+  sourcemap: true,
+  clean: true,
+};
+
+export default defineConfig([
+  {
+    ...commonConfig,
+    entry: ["src/index.ts"],
+    outDir: "dist",
+  },
+  {
+    ...commonConfig,
+    entry: ["src/shopify/index.ts"],
+    outDir: "shopify",
+  },
+  {
+    ...commonConfig,
+    entry: ["src/sanity/index.ts"],
+    outDir: "sanity",
+  },
+]);
