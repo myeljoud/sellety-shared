b6b1c1fbb91b43adeb2cf3b2417350e88b5aee4a
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2024-02-11 21:44:31 +0000

    patch

--- src/shopify/queries/index.ts
+++ src/shopify/queries/index.ts
@@ -3,3 +3,6 @@ export * from "./admin";
 export * from "./get-customer-address";
 export * from "./get-customer-orders";
 export * from "./get-order";
+export * from "./get-shipping-rates";
+export * from "./get-product";
+export * from "./get-collection-products";
