51360f2e0c4cc7370738bd444feda48d06b2031c
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2024-02-16 00:30:15 +0000

    patch

---
+++ .changeset/real-ligers-sparkle.md
@@ -0,0 +1,5 @@
+---
+"sellety-shared": patch
+---
+
+Exporting `staticMapsUrlCreator` and `StaticMapsUrlCreatorOptions`
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./type-guards";
 export * from "./utils";
 export * from "./constants";
 export * from "./types";
+export * from "./maps";
 export { CodeArea, OpenLocationCode } from "./openlocationcode";
 export { swapSanityProductsWithShopifysInModules } from "./swap-sanity-products-with-shopifys-in-modules";
